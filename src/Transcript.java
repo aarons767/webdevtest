0766e7671cfff801287ad8e2933c4a16a353dcfa
jacka <jacka@10.0.0.31>
2022-11-21 20:39:48 -0800

    finished testing basic request/response for api

---
+++ src/Transcript.java
@@ -0,0 +1,79 @@
+
+public class Transcript {
+		private String audio_url;
+		private String id;
+		private String status;
+		private String text;
+		private String language_model;
+
+		/**
+		 * @return the audio_url
+		 */
+		public String getAudio_url() {
+			return audio_url;
+		}
+
+		/**
+		 * @param audio_url the audio_url to set
+		 */
+		public void setAudio_url(String audio_url) {
+			this.audio_url = audio_url;
+		}
+
+		/**
+		 * @return the iD
+		 */
+		public String getID() {
+			return id;
+		}
+
+		/**
+		 * @param iD the iD to set
+		 */
+		public void setID(String iD) {
+			id = iD;
+		}
+
+		/**
+		 * @return the status
+		 */
+		public String getStatus() {
+			return status;
+		}
+
+		/**
+		 * @param status the status to set
+		 */
+		public void setStatus(String status) {
+			this.status = status;
+		}
+
+		/**
+		 * @return the text
+		 */
+		public String getText() {
+			return text;
+		}
+
+		/**
+		 * @param text the text to set
+		 */
+		public void setText(String text) {
+			this.text = text;
+		}
+
+		/**
+		 * @return the language_model
+		 */
+		public String getLanguage_model() {
+			return language_model;
+		}
+
+		/**
+		 * @param language_model the language_model to set
+		 */
+		public void setLanguage_model(String language_model) {
+			this.language_model = language_model;
+		}
+		
+}
